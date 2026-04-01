a916545aa5f34d0e19b00c9878d9b1c17417d115
Uchiha Shadow <shadowu876@gmail.com>
2026-04-02 04:04:05 +0630

    Add BOT_NAME environment variable to config

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 APIFY_API_TOKEN = os.getenv("APIFY_API_TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-ADMIN_GROUP_ID = os.getenv("ADMIN_GROUP_ID", "0") # ဒီစာကြောင်း ပါရပါမယ်
+ADMIN_GROUP_ID = os.getenv("ADMIN_GROUP_ID", "0")
+BOT_NAME = os.getenv("BOT_NAME", "Shadow Game Shop") # ဒီစာကြောင်းကို ထပ်ဖြည့်ပေးပါ
+
